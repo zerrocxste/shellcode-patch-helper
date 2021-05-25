0fd1481b5a71122bf66b5ecc117126b88e91270f
zerrocxste <49279293+zerrocxste@users.noreply.github.com>
2021-05-25 14:38:43 +0600

    Update includes.h

--- example_hook/shellcode-patch-example/src/includes.h
+++ example_hook/shellcode-patch-example/src/includes.h
@@ -1,13 +1,5 @@
 #include <iostream>
 #include <Windows.h>
 
-#ifdef _WIN64
-#define DWORD_OF_BITNESS DWORD64
-#define PTRMAXVAL ((PVOID)0x000F000000000000)
-#elif _WIN32
-#define DWORD_OF_BITNESS DWORD
-#define PTRMAXVAL ((PVOID)0xFFF00000)
-#endif
-
 #include "utilites/utilites.h"
 #include "shellcode_patch_helper/shellcode_patch_helper.h"
